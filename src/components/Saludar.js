5e374d309c486b67dd0de5d24cfd7dfe6a17118d
Luis García <luis_grdz@hotmail.com>
2020-03-24 18:16:04 -0600

    Video 18: props por defecto

--- src/components/Saludar.js
+++ src/components/Saludar.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 function Saludar(props){
     const {userInfo, saludarFn} = props;
+    const {nombre = "Anonimo"} = userInfo;
     
     return (
         <div>
             <h1>Hola {userInfo.nombre} tiene {userInfo.edad} años</h1>
 
-            <button onClick={() => saludarFn(userInfo.nombre)}>Saludar</button>
+            <button onClick={() => saludarFn(nombre)}>Saludar</button>
         </div>
     );
 }
