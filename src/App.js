bce85816c3ba569ec7b97202fd774a9e93bc6d5b
Luis García <luis_grdz@hotmail.com>
2020-03-24 18:49:55 -0600

    Video 21: añadiendo react bootstrap al proyecto

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Saludar from './components/Saludar';
+import { Button, Accordion, Card } from 'react-bootstrap';
 
 function App() {
   const user = {
@@ -24,6 +25,33 @@ function App() {
         </p>
 
         <Saludar userInfo = {user} saludarFn = {saludarFn}/>
+
+        <h1>React Bootstrap</h1>
+        <Button variant="primary">Primary</Button>
+
+        <h1>Acordion</h1>
+        <Accordion defaultActiveKey="0">
+          <Card>
+            <Card.Header>
+              <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                Click me!
+              </Accordion.Toggle>
+            </Card.Header>
+            <Accordion.Collapse eventKey="0">
+              <Card.Body>Hello! I'm the body</Card.Body>
+            </Accordion.Collapse>
+          </Card>
+          <Card>
+            <Card.Header>
+              <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                Click me!
+              </Accordion.Toggle>
+            </Card.Header>
+            <Accordion.Collapse eventKey="1">
+              <Card.Body>Hello! I'm another body</Card.Body>
+            </Accordion.Collapse>
+          </Card>
+        </Accordion>
       </header>
     </div>
   );
